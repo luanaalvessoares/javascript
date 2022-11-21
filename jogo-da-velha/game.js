0fe4599b7655f1d3ea7cd16bb40f76eb035730ad
Luana Alves <luanaalvessoares_@outlook.com>
2022-11-21 10:04:41 -0300

    feat: game pieces already fill the whole board

--- jogo-da-velha/game.js
+++ jogo-da-velha/game.js
@@ -1,3 +1,9 @@
 let stage = ['', '', '', '', '', '', '', '', '', '',];
 let player = 0;
 let pieces = ['o', 'x'];
+
+function handleMove(position) {
+    stage[position] = pieces[player];
+
+    player == 0 ? player = 1 : player = 0;
+}
--- jogo-da-velha/interface.js
+++ jogo-da-velha/interface.js
@@ -7,5 +7,20 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 function handleClick(event) {
-    console.log(event.target)
+    let square = event.target;
+    let position = square.id;
+
+    handleMove(position);
+    updateSquares();
+}
+
+function updateSquares() {
+    let squares = document.querySelectorAll('.square');
+
+    squares.forEach(element => {
+        let position = element.id;
+        let piece = stage[position];
+
+        if (piece !== '') { element.innerHTML = `<div class="${piece}"></div>`; }
+    })
 }
