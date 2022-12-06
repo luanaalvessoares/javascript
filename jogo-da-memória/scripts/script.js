4f988265037964e107fe2f66120326a900225e47
Luana Alves <luanaalvessoares_@outlook.com>
2022-12-05 21:19:59 -0300

    feat: creating animation of cards

--- jogo-da-memória/scripts/script.js
+++ jogo-da-memória/scripts/script.js
@@ -13,6 +13,9 @@ function initializeCards(cards) {
         cardElement.dataset.icon = card.icon;
         
         createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard)
+        gameBoard.appendChild(cardElement);
     })
 }
 
@@ -33,4 +36,28 @@ function createCardFace(face, card, element) {
         cardElementFace.innerHTML = "&lt/&gt";
     }
     element.appendChild(cardElementFace);
+}
+
+function flipCard() {
+    if (game.setCard(this.id)) {
+        this.classList.add("flip");
+        if (game.secondCard) {
+            if (game.checkMatch()) {
+                game.clearCards();
+                if (game.checkGameOver()) {
+                    let gameOverLayer = document.getElementById("gameOver");
+                    gameOverLayer.style.display = 'flex';
+                }
+            } else {
+                setTimeout(() => {
+                    let firstCardView = document.getElementById(game.firstCard.id);
+                    let secondCardView = document.getElementById(game.secondCard.id);
+
+                    firstCardView.classList.remove('flip');
+                    secondCardView.classList.remove('flip');
+                    game.unflipCards();
+                }, 1000);
+            };
+        }
+    }
 }
